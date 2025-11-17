3ca77c617779adadc12e3494f88046c68b8f26f9
Archanagupta20 <archanagupta1879@gmail.com>
2025-11-17 15:25:29 +0530

    Add files via upload

---
+++ Order.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  products: [{ productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: Number }],
+  totalPrice: Number,
+  status: { type: String, default: 'Pending' },
+  orderDate: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
